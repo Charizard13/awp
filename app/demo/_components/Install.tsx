c4916c0fb8b8dfeb6d6fa367c01c829f7c3a2b39
Tal Piven <67857533+Charizard13@users.noreply.github.com>
2023-11-08 23:12:00 +0200

    refactor install demo

---
+++ app/demo/_components/Install.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { toast } from "@/components/ui/use-toast";
+import { Button } from "@/components/ui/button";
+
+import useInstall from "@/hooks/useInstall";
+
+export default function Install() {
+  const { openInstallDialog, status } = useInstall();
+  const handleCopy = () => {
+    navigator.clipboard.writeText(window.location.href);
+    toast({
+      title: "Copied to clipboard",
+      description: "Use the copied URL to install the app on your device/computer",
+    });
+  };
+  switch (status) {
+    case "idle":
+      return (
+        <Button className="px-6 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 " variant="default" onClick={openInstallDialog}>
+          Install App <span className="material-symbols-outlined">get_app</span>
+        </Button>
+      );
+
+    case "installing":
+      return (
+        <div className="border border-gray-200 dark:border-gray-700 p-4 rounded-md">
+          <p className="text-sm text-gray-600 dark:text-gray-400">Your app is installing...</p>
+        </div>
+      );
+
+    case "installed":
+      return (
+        <div className="border border-gray-200 dark:border-gray-700 p-4 rounded-md">
+          <p className="text-sm text-gray-600 dark:text-gray-400">Your app is installed!</p>
+        </div>
+      );
+
+    case "unSupported":
+    default:
+      return (
+        <div className="text-foreground">
+          <div className="space-y-4">
+            <h2 className="text-2xl font-semibold items-center flex space-x-2">
+              <span className="material-symbols-outlined text-red-500">warning</span> Your browser is not supported
+            </h2>
+            <h3 className="text-mdfont-semibold items-center flex space-x-2">Follow the steps below to install the app on your device/computer</h3>
+            <ol className="list-decimal list-inside text-gray-600 dark:text-gray-400 space-y-8">
+              <li>
+                <Button className="px-6 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600" onClick={handleCopy}>
+                  Copy the URL of this page
+                  <span className="material-symbols-outlined">content_copy</span>
+                </Button>
+              </li>
+              <li className="mb-2">
+                <a href="https://www.google.com/chrome/" className="text-foreground underline" target="_blank" rel="noopener noreferrer">
+                  Go to official Chrome website download page
+                </a>
+              </li>
+              <li>Open Google Chrome.</li>
+              <li>Paste the URL in the address bar and press Enter.</li>
+            </ol>
+          </div>
+        </div>
+      );
+  }
+}
--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,8 +1,5 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { toast } from "@/components/ui/use-toast";
-import useInstall from "@/hooks/useInstall";
 import type { App } from "@/hooks/useInstall";
+import Install from "./_components/Install";
 declare global {
   interface Window {
     showOpenFilePicker(): Promise<any>;
@@ -13,61 +10,11 @@ declare global {
 }
 
 export default function DemoPage() {
-  const { openInstallDialog, status } = useInstall();
-  const handleCopy = () => {
-    navigator.clipboard.writeText(window.location.href);
-    toast({
-      title: "Copied to clipboard",
-      description: "Use the copied URL to install the app on your device/computer",
-    });
-  };
-
   return (
     <div className="flex flex-col items-center justify-center flex-grow text-center p-4 space-y-8">
       <h1 className="text-3xl font-bold">Awp Demo Page</h1>
       <p className="text-lg text-gray-600 dark:text-gray-400">Explore the features and capabilities of our Progressive Web App (PWA).</p>
-
-      {status === "idle" && (
-        <Button className="px-6 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 " variant="default" onClick={openInstallDialog}>
-          Install App <span className="material-symbols-outlined">get_app</span>
-        </Button>
-      )}
-      {status === "installing" && (
-        <div className="border border-gray-200 dark:border-gray-700 p-4 rounded-md">
-          <p className="text-sm text-gray-600 dark:text-gray-400">Your app is installing...</p>
-        </div>
-      )}
-
-      {status === "installed" && (
-        <div className="border border-gray-200 dark:border-gray-700 p-4 rounded-md">
-          <p className="text-sm text-gray-600 dark:text-gray-400">Your app is installed!</p>
-        </div>
-      )}
-      {status === "unSupported" && (
-        <div className="text-foreground">
-          <div className="space-y-4">
-            <h2 className="text-2xl font-semibold items-center flex space-x-2">
-              <span className="material-symbols-outlined text-red-500">warning</span> Your browser is not supported
-            </h2>
-            <h3 className="text-mdfont-semibold items-center flex space-x-2">Follow the steps below to install the app on your device/computer</h3>
-            <ol className="list-decimal list-inside text-gray-600 dark:text-gray-400 space-y-8">
-              <li>
-                <Button className="px-6 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600" onClick={handleCopy}>
-                  Copy the URL of this page
-                  <span className="material-symbols-outlined">content_copy</span>
-                </Button>
-              </li>
-              <li className="mb-2">
-                <a href="https://www.google.com/chrome/" className="text-foreground underline" target="_blank" rel="noopener noreferrer">
-                  Go to official Chrome website download page
-                </a>
-              </li>
-              <li>Open Google Chrome.</li>
-              <li>Paste the URL in the address bar and press Enter.</li>
-            </ol>
-          </div>
-        </div>
-      )}
+      <Install />
     </div>
   );
 }
