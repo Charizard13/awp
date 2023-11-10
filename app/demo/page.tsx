0f87d470dd68e1de1242ba9abca01e964286bb3b
Tal Piven <67857533+Charizard13@users.noreply.github.com>
2023-11-10 14:05:05 +0200

    remove useservice

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -2,7 +2,7 @@
 import type { App } from "@/hooks/useInstall";
 import Install from "./_components/Install";
 import { Button } from "@/components/ui/button";
-import useServiceWorker from "@/hooks/useServiceWorker";
+// import useServiceWorker from "@/hooks/useServiceWorker";
 declare global {
   interface Window {
     showOpenFilePicker?(): Promise<any>;
@@ -14,13 +14,13 @@ declare global {
 }
 
 export default function DemoPage() {
-  const { isRegistered, unRegister, register } = useServiceWorker();
+  // const { isRegistered, unRegister, register } = useServiceWorker();
   return (
     <div className="flex flex-col items-center justify-center flex-grow text-center p-4 space-y-8">
       <h1 className="text-3xl font-bold">Awp Demo Page</h1>
       <p className="text-lg text-gray-600 dark:text-gray-400">Explore the features and capabilities of our Progressive Web App (PWA).</p>
       <Install />
-      <div className="flex flex-col items-center justify-center flex-grow text-center p-4 space-y-8">
+      {/* <div className="flex flex-col items-center justify-center flex-grow text-center p-4 space-y-8">
         <Button onClick={register}>Register Service Worker</Button>
         <Button onClick={unRegister} variant="outline">
           Unregister Service Worker
@@ -32,7 +32,7 @@ export default function DemoPage() {
             <p className="text-lg text-gray-600 dark:text-gray-400">Service Worker is not registered</p>
           )}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
