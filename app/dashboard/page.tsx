6f1ea78cd4d855e8f3a0e8f7654716f44fec3da6
Tal Piven <67857533+Charizard13@users.noreply.github.com>
2023-11-16 16:03:46 +0200

    added basic loaders

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,6 @@
 import Info from "./_components/Info";
 
 export default function DashBoardPage() {
-  throw new Error("Something went wrong");
   return (
     <div className="flex flex-col items-center justify-center w-full h-full space-y-4">
       <h1 className="text-4xl font-bold">Dashboard</h1>
