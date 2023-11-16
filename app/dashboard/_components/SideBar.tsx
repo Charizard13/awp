e75997e00782562f4caa2d71d80b4fe3cb3e305d
Tal Piven <67857533+Charizard13@users.noreply.github.com>
2023-11-16 17:34:28 +0200

    fix app bar

--- app/dashboard/_components/SideBar.tsx
+++ app/dashboard/_components/SideBar.tsx
@@ -2,7 +2,7 @@ import { Separator } from "@/components/ui/separator";
 import { ClipboardIcon, HomeIcon, PaletteIcon, SendToBackIcon, SettingsIcon } from "lucide-react";
 import Link from "next/link";
 
-const navigation = [
+export const dashboardRoutes = [
   {
     name: "Home",
     href: "/dashboard" as const,
@@ -33,7 +33,7 @@ const navigation = [
 export default function SideBar() {
   return (
     <ul className="flex flex-col md:gap-2 border-r border-gray-300 p-2 md:p-4">
-      {navigation.map((item) => (
+      {dashboardRoutes.map((item) => (
         <li key={item.name}>
           <Link href={item.href} className="flex items-center py-2 px-2 text-gray-600 dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-md md:px-4">
             <span className="mx-2 md:mx-4 text-lg font-normal">
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,7 +2,8 @@ import Link from "next/link";
 import AuthButton from "../AuthButton";
 import { Button } from "@/components/ui/button";
 import { CloudIcon } from "lucide-react";
-
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
+import { dashboardRoutes } from "@/app/dashboard/_components/SideBar";
 export default function Header() {
   return (
     <header className="flex items-center justify-between p-4 bg-white dark:bg-gray-800 h-16 shadow-sm">
@@ -10,9 +11,28 @@ export default function Header() {
         <CloudIcon />
         <h1 className="ml-2 text-2xl font-semibold text-gray-900 dark:text-gray-100">Awp</h1>
       </Link>
-      <Button className="hidden md:block" asChild variant="ghost">
-        <Link href="/dashboard">Dashboard</Link>
-      </Button>
+      <HoverCard openDelay={100}>
+        <HoverCardTrigger className="hidden md:block">
+          <Button variant="ghost">Dashboard</Button>
+        </HoverCardTrigger>
+        <HoverCardContent>
+          <li className="flex flex-col gap-2">
+            {dashboardRoutes.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className="flex items-center py-2 px-2 text-gray-600 dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-md md:px-4"
+              >
+                <span className="mx-2 md:mx-4 text-lg font-normal">
+                  <item.icon />
+                </span>
+                <span className="hidden md:block">{item.name}</span>
+              </Link>
+            ))}
+          </li>
+        </HoverCardContent>
+      </HoverCard>
+
       <AuthButton />
     </header>
   );
