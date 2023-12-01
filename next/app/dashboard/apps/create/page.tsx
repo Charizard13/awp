6ade5794003bf8806cf4c5e45ce8b263a05e801d
Tal Piven <67857533+Charizard13@users.noreply.github.com>
2023-12-01 12:20:52 +0200

    Added local auth

--- next/app/dashboard/apps/create/page.tsx
+++ next/app/dashboard/apps/create/page.tsx
@@ -1,11 +1,4 @@
-import {
-  CardTitle,
-  CardDescription,
-  CardHeader,
-  CardContent,
-  CardFooter,
-  Card,
-} from "@/components/ui/card";
+import { CardTitle, CardDescription, CardHeader, CardContent, CardFooter, Card } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
@@ -17,11 +10,7 @@ import { generateScript } from "@/lib/pwa/script";
 import { appAssets } from "@/lib/consts";
 import SubmitButton from "@/components/SubmitButton";
 
-export default function CreateAppPage({
-  searchParams,
-}: {
-  searchParams: { message: string };
-}) {
+export default function CreateAppPage({ searchParams }: { searchParams: { message: string } }) {
   const handleSubmit = async (formData: FormData) => {
     "use server";
 
@@ -34,12 +23,10 @@ export default function CreateAppPage({
     const supabase = createServerClient(cookieStore);
     const user = await supabase.auth.getUser();
     const userId = user?.data?.user?.id;
-    const route = "/dashboard/createApp";
+    const route = "/dashboard/apps/create";
 
     if (!userId) {
-      return redirect(
-        `${route}?message=You must be logged in to create metadata.`,
-      );
+      return redirect(`${route}?message=You must be logged in to create metadata.`);
     }
 
     const { error, data: app } = await supabase
@@ -54,35 +41,21 @@ export default function CreateAppPage({
       .single();
 
     if (error) {
-      return redirect(
-        `${route}?message=There was an error creating your metadata.`,
-      );
+      return redirect(`${route}?message=There was an error creating your metadata.`);
     }
 
     const pngIcon = new Blob([icon], { type: "image/png" });
-    const uploadIcon = supabase.storage
-      .from("apps")
-      .upload(`${app.id}/${appAssets.icon}`, pngIcon);
+    const uploadIcon = supabase.storage.from("apps").upload(`${app.id}/${appAssets.icon}`, pngIcon);
     const appManifest = generateManifest(app, pngIcon.type);
-    const uploadManifest = supabase.storage
-      .from("apps")
-      .upload(`${app.id}/${appAssets.manifest}`, appManifest);
+    const uploadManifest = supabase.storage.from("apps").upload(`${app.id}/${appAssets.manifest}`, appManifest);
     const appScript = generateScript();
-    const uploadScript = supabase.storage
-      .from("apps")
-      .upload(`${app.id}/${appAssets.script}`, appScript);
+    const uploadScript = supabase.storage.from("apps").upload(`${app.id}/${appAssets.script}`, appScript);
 
-    const [iconData, manifestData, scriptData] = await Promise.all([
-      uploadIcon,
-      uploadManifest,
-      uploadScript,
-    ]);
+    const [iconData, manifestData, scriptData] = await Promise.all([uploadIcon, uploadManifest, uploadScript]);
 
     if (iconData.error || manifestData.error || scriptData.error) {
       await supabase.from("apps").delete().match({ id: app.id });
-      return redirect(
-        `${route}?message=There was an error uploading your metadata.`,
-      );
+      return redirect(`${route}?message=There was an error uploading your metadata.`);
     }
 
     redirect("/dashboard");
@@ -100,53 +73,23 @@ export default function CreateAppPage({
             <div className="space-y-4">
               <div className="space-y-2">
                 <Label htmlFor="icon-upload">Icon</Label>
-                <Input
-                  accept="image/*"
-                  id="icon-upload"
-                  type="file"
-                  name="icon"
-                />
-                <p className="text-sm text-zinc-500 dark:text-zinc-400">
-                  Upload your app&apos;s icon.
-                </p>
+                <Input accept="image/*" id="icon-upload" type="file" name="icon" />
+                <p className="text-sm text-zinc-500 dark:text-zinc-400">Upload your app&apos;s icon.</p>
               </div>
               <div className="space-y-2">
                 <Label htmlFor="app-name">Name</Label>
-                <Input
-                  id="app-name"
-                  placeholder="App Name"
-                  required
-                  name="name"
-                />
+                <Input id="app-name" placeholder="App Name" required name="name" />
               </div>
               <div className="space-y-2">
                 <Label htmlFor="app-url">URL</Label>
-                <Input
-                  id="app-url"
-                  placeholder="https://example.com"
-                  required
-                  name="url"
-                />
-                <p className="text-sm text-zinc-500 dark:text-zinc-400">
-                  Enter the link of your website.
-                </p>
+                <Input id="app-url" placeholder="https://example.com" required name="url" />
+                <p className="text-sm text-zinc-500 dark:text-zinc-400">Enter the link of your website.</p>
               </div>
               <div className="space-y-2">
-                <Label htmlFor="app-description">
-                  App Description (Optional)
-                </Label>
-                <Textarea
-                  className="min-h-[100px]"
-                  id="app-description"
-                  placeholder="Describe your app"
-                  name="description"
-                />
+                <Label htmlFor="app-description">App Description (Optional)</Label>
+                <Textarea className="min-h-[100px]" id="app-description" placeholder="Describe your app" name="description" />
               </div>
-              {searchParams?.message && (
-                <p className="mt-4 p-4 rounded-sm  bg-foreground/10 text-foreground text-center">
-                  {searchParams.message}
-                </p>
-              )}
+              {searchParams?.message && <p className="mt-4 p-4 rounded-sm  bg-foreground/10 text-foreground text-center">{searchParams.message}</p>}
             </div>
           </CardContent>
           <CardFooter>
