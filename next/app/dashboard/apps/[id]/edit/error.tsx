877c9c9bf240ec5afb39daf76cd915e41a52ac15
Tal Piven <67857533+Charizard13@users.noreply.github.com>
2023-12-07 12:39:59 +0200

    added edit

---
+++ next/app/dashboard/apps/[id]/edit/error.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { Card, CardContent, CardHeader, CardFooter } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Error() {
+  return (
+    <Card className="flex flex-col items-center justify-center w-full max-w-lg h-full m-auto">
+      <CardHeader>
+        <h1 className="text-4xl font-bold">Could not find your app</h1>
+      </CardHeader>
+      <CardContent>
+        <p className="text-xl">There was an error getting your app metadata.</p>
+      </CardContent>
+      <CardFooter>
+        <Button asChild>
+          <Link href="/dashboard/apps">Visit Dashboard</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
--- next/components/SubmitButton.tsx
+++ next/components/SubmitButton.tsx
@@ -4,24 +4,29 @@ import { Loader2Icon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { useFormStatus } from "react-dom";
+import { useEffect, useState, useId } from "react";
 
 type SubmitButtonProps = {
   children: React.ReactNode;
   [key: string]: unknown;
 };
 
-export default function SubmitButton({
-  children,
-  ...props
-}: SubmitButtonProps) {
+export default function SubmitButton({ children, ...props }: SubmitButtonProps) {
   const { pending } = useFormStatus();
+  const id = useId();
+  const [childrenWidth, setChildrenWidth] = useState(0);
+
+  useEffect(() => {
+    if (!pending) {
+      const buttonElement = document.getElementById(id);
+      if (!buttonElement) return;
+      const childrenWidth = buttonElement.offsetWidth;
+      setChildrenWidth(childrenWidth);
+    }
+  }, [pending, id]);
 
   return (
-    <Button
-      className="flex items-center justify-center"
-      {...props}
-      type="submit"
-    >
+    <Button className="flex items-center justify-center" {...props} type="submit" id={id} style={{ minWidth: childrenWidth }}>
       {pending ? <Loader2Icon className="animate-spin" /> : children}
     </Button>
   );
