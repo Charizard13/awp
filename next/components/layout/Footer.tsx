4dc52ab2fe0743b0007fddd5b28ef97ba199acad
Tal Piven <67857533+Charizard13@users.noreply.github.com>
2023-12-07 13:41:29 +0200

    a

--- next/components/layout/Footer.tsx
+++ next/components/layout/Footer.tsx
@@ -1,8 +1,4 @@
-"use client";
-
 export default function Footer() {
-  console.log(navigator.userAgent);
-
   return (
     <footer className="flex items-center justify-center h-16 bg-gray-100 dark:bg-gray-900">
       <p className="text-sm text-gray-600 dark:text-gray-400">© 2023 Awp. All rights reserved.</p>
