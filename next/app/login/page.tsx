38049cab5c058f5d8d2ad8f269f4d914bc559f0d
Tal Piven <67857533+Charizard13@users.noreply.github.com>
2023-12-07 15:04:10 +0200

    added google loding button

--- next/app/login/page.tsx
+++ next/app/login/page.tsx
@@ -2,14 +2,11 @@ import { cookies } from "next/headers";
 import { createServerClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
 import { Provider } from "@supabase/supabase-js";
-import { Button } from "@/components/ui/button";
 import { getDefaultUrl } from "@/lib/url";
+import SubmitButton from "@/components/SubmitButton";
+import GoogleIcon from "./_components/GoogleIcon";
 
-export default function Login({
-  searchParams,
-}: {
-  searchParams: { message: string };
-}) {
+export default function Login({ searchParams }: { searchParams: { message: string } }) {
   const signIn = async (formData: FormData) => {
     "use server";
 
@@ -31,20 +28,13 @@ export default function Login({
 
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <form
-        className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-        action={signIn}
-      >
+      <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground" action={signIn}>
         <h1 className="text-3xl font-semibold">Login</h1>
         <h2 className="text-xl font-semibold">Welcome to Awp</h2>
-        <Button variant="outline" type="submit" name="provider" value="google">
-          Google
-        </Button>
-        {searchParams?.message && (
-          <p className="mt-4 p-4 rounded-md bg-foreground/10 text-foreground text-center">
-            {searchParams.message}
-          </p>
-        )}
+        <SubmitButton variant="outline" name="provider" value="google">
+          <GoogleIcon className="w-6 h-6 mr-2" /> Google
+        </SubmitButton>
+        {searchParams?.message && <p className="mt-4 p-4 rounded-md bg-foreground/10 text-foreground text-center">{searchParams.message}</p>}
       </form>
     </div>
   );
