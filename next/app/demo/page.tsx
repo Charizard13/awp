7400516eb757fbadbf3bbfd6490abdedb24c3224
Tal Piven <67857533+Charizard13@users.noreply.github.com>
2023-12-13 21:45:33 +0200

    a

--- next/app/demo/page.tsx
+++ next/app/demo/page.tsx
@@ -1,12 +1,4 @@
 "use client";
-import { Button } from "@/components/ui/button";
-// import useNotifications from "@/hooks/useNotifications";
-import { DownloadIcon } from "lucide-react";
-import { useEffect, useState } from "react";
-// import { runBanner } from "@/components/web/InstallBanner";
-import { AddToHomeScreen } from "@/components/web/AddTohomeScreen";
-import { AddToDock } from "@/components/web/AddToDock";
-import { InstallButton } from "@/components/web/InstallButton";
 
 type App = {
   platform: AppPlatform;
@@ -58,33 +50,16 @@ interface InstallButtonAttributes extends React.HTMLAttributes<HTMLElement> {
 }
 
 export default function DemoPage() {
-  // const { openNotificationsDialog } = useNotifications();
-  useEffect(() => {
-    const installButton = new InstallButton();
-    // const addToDock = new AddToDock();
-    // const addToHomeScreen = new AddToHomeScreen();
-    // const installBanner = new InstallBanner();
-    // document.body.appendChild(installBanner);
-  }, []);
-
   return (
     <div className="flex flex-grow flex-col items-center justify-center space-y-8 p-4 text-center">
       <h1 className="text-3xl font-bold">Awp Demo Page</h1>
       <p className="text-lg text-gray-600 dark:text-gray-400">
         Explore the features and capabilities of our Progressive Web App (PWA).
       </p>
-      <install-button>
-        <Button>
-          Install App
-          <DownloadIcon className="ml-2 h-4 w-4" />
-        </Button>
-      </install-button>
+
       <install-button className="bg-blue-500 hover:bg-blue-600">
         Install App
       </install-button>
-
-      {/* <add-to-dock></add-to-dock> */}
-      {/* <Button onClick={openNotificationsDialog}>Open Notifications Dialog</Button> */}
     </div>
   );
 }
