f504d56edd678096ccc5f802e43da100658b7fea
Tal Piven <67857533+Charizard13@users.noreply.github.com>
2023-12-19 22:13:18 +0200

    finish side bar ui

--- next/app/dashboard/brands/_components/SideBar.tsx
+++ next/app/dashboard/brands/_components/SideBar.tsx
@@ -1,15 +1,9 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import { Separator } from "@/components/ui/separator";
-import {
-  ClipboardIcon,
-  HomeIcon,
-  PaletteIcon,
-  SendToBackIcon,
-  SettingsIcon,
-} from "lucide-react";
+import { HomeIcon, SendToBackIcon, SettingsIcon } from "lucide-react";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function SideBar() {
   const params = useParams();
@@ -17,11 +11,12 @@ export default function SideBar() {
 
   const dashboardRoutes = [
     {
-      name: "My brands",
+      name: "Brands",
       href: "/dashboard/brands" as const,
       icon: HomeIcon,
     },
   ];
+
   if (brandId) {
     dashboardRoutes.push({
       name: "Profile",
@@ -37,18 +32,17 @@ export default function SideBar() {
     });
   }
   return (
-    <ul className="flex flex-col border-r border-gray-300 p-2 md:gap-2 md:p-4">
+    <ul className="flex flex-col gap-4 border-r border-gray-300 p-2 md:p-4">
       {dashboardRoutes.map((item) => (
         <li key={item.name}>
-          <Link
-            href={item.href}
-            className="flex items-center rounded-md px-2 py-2 text-gray-600 hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 md:px-4"
-          >
-            <span className="mx-2 font-normal">
-              <item.icon />
-            </span>
-            <span className="hidden md:block">{item.name}</span>
-          </Link>
+          <Button asChild variant={"ghost"} className="flex w-full gap-2">
+            <Link href={item.href}>
+              <item.icon className="h-6 w-6" />
+              <span className="hidden flex-1 text-lg md:block">
+                {item.name}
+              </span>
+            </Link>
+          </Button>
         </li>
       ))}
     </ul>
--- next/app/dashboard/layout.tsx
+++ next/app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex w-full flex-grow flex-row">
-      <div className="flex-grow p-2"> {children}</div>
+      <div className="flex-grow"> {children}</div>
     </div>
   );
 }
