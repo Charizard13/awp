58c10a2e8784dfdad1f49f7976d332649e319c3e
TalPiven13 <157593149+TalPiven13@users.noreply.github.com>
2024-04-02 15:12:49 +0300

    updated supabase middleware

--- next/middleware.ts
+++ next/middleware.ts
@@ -1,39 +1,19 @@
-import { NextResponse, type NextRequest } from "next/server";
-import { createMiddlewareClient } from "@/lib/supabase/middleware";
-
-const protectedRoutes = ["/dashboard"];
+import { type NextRequest } from 'next/server'
+import { updateSession } from '@/lib/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
-  try {
-    // This `try/catch` block is only here for the interactive tutorial.
-    // Feel free to remove once you have Supabase connected.
-    const { supabase, response } = createMiddlewareClient(request);
-
-    // Refresh session if expired - required for Server Components
-    // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
-    const { data } = await supabase.auth.getSession();
-    const userId = data.session?.user.id;
-    const redirect = isAuthorized(request, userId);
-    if (redirect) {
-      const message = `You must be signed in to access ${request.nextUrl.pathname}`;
-      return NextResponse.redirect(
-        new URL(`/login?message=${message}`, request.url),
-      );
-    }
-  } catch (e) {
-    // If you are here, a Supabase client could not be created!
-    // This is likely because you have not set up environment variables.
-    // Check out http://localhost:3000 for Next Steps.
-    return NextResponse.next({
-      request: {
-        headers: request.headers,
-      },
-    });
-  }
+  return await updateSession(request)
 }
 
-function isAuthorized(request: NextRequest, userId: string | undefined) {
-  return protectedRoutes.some(
-    (route) => request.nextUrl.pathname.startsWith(route) && !userId,
-  );
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
 }
